80a54a9d8f844abf77d204d8acec36fe823c2eb0
Camilla <camilla.andrade.pereira@gmail.com>
2025-06-13 17:56:56 -0300

    add script para camera fixa

---
+++ Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FixedCamera : MonoBehaviour
+{
+    public int pixelsPerUnit = 32;
+    public int tilesVertical = 14;
+
+    private Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        cam.orthographic = true;
+        // orthographicSize é metade da altura em unidades (1 tile = 1 unidade)
+        cam.orthographicSize = tilesVertical / 2f;
+    }
+
+    void LateUpdate()
+    {
+        // pixel perfect na movimentação da câmera
+        float unitsPerPixel = 1f / pixelsPerUnit;
+        Vector3 pos = cam.transform.position;
+
+        pos.x = Mathf.Round(pos.x / unitsPerPixel) * unitsPerPixel;
+        pos.y = Mathf.Round(pos.y / unitsPerPixel) * unitsPerPixel;
+
+        cam.transform.position = pos;
+    }
+}
